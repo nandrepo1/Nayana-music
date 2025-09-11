340d2a7ad410a6e83af713dfa1c0a803c5629596
nandrepo1 <github25u7@gmail.com>
2025-09-11 21:05:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -49,7 +49,7 @@ AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
 
 API_URL = getenv("API_URL", 'https://api.thequickearn.xyz') #youtube song url
 VIDEO_API_URL = getenv("VIDEO_API_URL", 'https://api.video.thequickearn.xyz')
-API_KEY = getenv("API_KEY", 'NxGBNexGenBots267382') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
+API_KEY = getenv("API_KEY", 'NxGBNexGenBotse2fcb7') # youtube song api key, generate free key or buy paid plan from panel.thequickearn.xyz
 
 
 # Get this credentials from https://developer.spotify.com/dashboard
